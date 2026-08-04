a146fd9917670660bf39ac14fc0d3620ef795bd2
C Harinath Reddy <Harireddy9050@gmail.com>
2026-08-04 12:52:55 +0530

    new files

---
+++ track/M02/T02/practice/p1.java
@@ -0,0 +1,14 @@
+
+class p1 {
+
+    public static void main(String[] args) {
+        int num = -26;
+        if (num == 0) {
+            System.out.println("Zero");
+        } else if (num > 0) {
+            System.out.println("Positive");
+        } else {
+            System.out.println("Negative");
+        }
+    }
+}
