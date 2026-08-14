edbfab337ec8a056d657e4d79f74ab32b782a91c
C Harinath Reddy <Harireddy9050@gmail.com>
2026-08-14 12:33:51 +0530

    added new files

---
+++ track/M02/TO6/prg1.java
@@ -0,0 +1,14 @@
+
+class Robot {
+
+    Robot() {
+        System.out.println("Beep beep! Robot reporting for Java duty!");
+    }
+}
+
+public class prg1 {
+
+    public static void main(String[] args) {
+        Robot r = new Robot();
+    }
+}
