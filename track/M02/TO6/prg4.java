8c82bdf95158f29da48aca5ec7aa9914a613660c
C Harinath Reddy <Harireddy9050@gmail.com>
2026-08-14 13:17:05 +0530

    added new files

---
+++ track/M02/TO6/prg4.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+class Book {
+
+    String title;
+    String author;
+
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    void display() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+    }
+}
+
+public class prg4 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Book b = new Book(scanner.nextLine(), scanner.nextLine());
+        b.display();
+    }
+}
---
+++ track/M02/TO6/prg6.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+
+class MobilePlan {
+
+    String planName;
+    int price;
+
+    MobilePlan() {
+        this.planName = "Basic";
+        this.price = 199;
+    }
+
+    MobilePlan(String planName, int price) {
+        this.planName = planName;
+        this.price = price;
+    }
+
+    void display() {
+        System.out.println("Plan: " + planName + ", Price: " + price);
+    }
+}
+
+public class prg6 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String planName = scanner.nextLine();
+        int price = scanner.nextInt();
+        MobilePlan m1 = new MobilePlan();
+        MobilePlan m2 = new MobilePlan(planName, price);
+        m1.display();
+        m2.display();
+    }
+}
