a1de4bf8017738829125ba3523a7f45fa1222138
C Harinath Reddy <Harireddy9050@gmail.com>
2026-08-17 12:15:18 +0530

    added new files

--- track/M02/T07/array.java
+++ track/M02/T07/array.java
@@ -1,12 +1,23 @@
 
+import java.util.Scanner;
+
 public class array {
 
     public static void main(String[] args) {
-        int a = 11;
-        int b = 12;
-        int c = 13;
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
+        // int a = 11;
+        // int b = 12;
+        // int c = 13;
+        // System.out.println(a);
+        // System.out.println(b);
+        // System.out.println(c);
+        Scanner sc = new Scanner(System.in);
+        int a[] = new int[5];
+        for (int i = 0; i < 5; i++) {
+            a[i] = sc.nextInt();
+        }
+        for (int i = 0; i < 5; i++) {
+            System.out.println(a[i]);
+        }
+
     }
 }
