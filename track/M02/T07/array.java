9a80dd2c2c87ee61a8d843c9f25333b6abd8c5d7
C Harinath Reddy <Harireddy9050@gmail.com>
2026-08-17 11:50:19 +0530

    added new files

---
+++ track/M02/T07/array.java
@@ -0,0 +1,12 @@
+
+public class array {
+
+    public static void main(String[] args) {
+        int a = 11;
+        int b = 12;
+        int c = 13;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
