65055c67db55d9de97d625bdf8235e189dbf839c
C Harinath Reddy <Harireddy9050@gmail.com>
2026-08-18 11:02:22 +0530

    new files

---
+++ track/M02/T07/arrayForLoop.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class arrayForLoop {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the 5 Array elements : ");
+        int a[] = new int[5];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = sc.nextInt();
+        }
+        int sum = 0;
+        System.out.print1("Arrays elements are : ");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+            sum += a[i];
+        }
+        System.out.println("");
+        System.out.println("Sum: " + sum);
+    }
+}
---
+++ track/M02/T07/prg5.java
@@ -0,0 +1,15 @@
+
+public class prg5 {
+
+    public static void main(String[] args) {
+        int[] marks = {70, 80, 60, 90, 50};
+        int sum = 0;
+        double avg;
+        for (int i = 0; i < marks.length; i++) {
+            sum += marks[i];
+        }
+        avg = (double) sum / marks.length;
+        System.out.println("Total: " + sum);
+        System.out.println("Average: " + avg);
+    }
+}
